ef27224e0fc70c4a3be3f7cc3a087bcd989c2006
Julian ⚡ <45518053+UtilityDev@users.noreply.github.com>
2024-01-08 00:16:52 +0100

    Add Block class

---
+++ DeeperDeeper_CS/src/Block.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+namespace DeeperDeeper_CS
+{
+    public class Block : Entity
+    {
+        public int length;
+        public float speed;
+
+        public Block(Vector2 pos, int length, float speed, Color color)
+        {
+            this.position   = pos;
+            this.length     = length;
+            this.speed      = speed;
+            this.color      = color;
+        }
+
+        public void Fall()
+        {
+            this.position.Y -= this.speed * GetFrameTime();
+        }
+
+        public override void Draw()
+        {
+            DrawRectangle((int)this.position.X, (int)this.position.Y, this.length, 20, this.color);
+        }
+    }
+}
