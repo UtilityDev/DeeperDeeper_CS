0a1ee10d56e3b0c8d03ff4209b86cb5d9961799b
Julian ⚡ <45518053+UtilityDev@users.noreply.github.com>
2024-01-07 18:46:01 +0100

    Removed and replaced Interfaces with Classes

---
+++ DeeperDeeper_CS/src/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+namespace DeeperDeeper_CS
+{
+    public class Entity
+    {
+        public Vector2 position = new Vector2();
+        public Color color;
+
+        public virtual void Draw()
+        {
+            DrawRectangle((int)position.X, (int)position.Y, 20, 20, Color.WHITE);
+        }
+    }
+}
--- DeeperDeeper_CS/src/Player.cs
+++ DeeperDeeper_CS/src/Player.cs
@@ -1,27 +1,30 @@
 ﻿using System.Numerics;
 using Raylib_cs;
 using static Raylib_cs.Raylib;
-using DeeperDeeper_CS.Interfaces;
 
 namespace DeeperDeeper_CS
 {
-    class Player : IEntity
+    class Player : Entity
     {
-        public Vector2 Position { get; set; }
-        public Color color;
-
         public float radius = 40.0f;
+        public float moveSpeed = 7.0f;
 
         public Player(Vector2 pos, float radius, Color color)
         {
-            this.Position = pos;
+            this.position = pos;
             this.radius = radius;
             this.color = color;
         }
 
-        public void Draw()
+        public void Move()
         {
-            DrawCircle((int)this.Position.X, (int)this.Position.Y, this.radius, this.color);
+            if (IsKeyDown(KeyboardKey.KEY_A)) { this.position.X -= this.moveSpeed; }
+            if (IsKeyDown(KeyboardKey.KEY_D)) { this.position.X += this.moveSpeed; }
+        }
+
+        public override void Draw()
+        {
+            DrawCircle((int)this.position.X, (int)this.position.Y, this.radius, this.color);
         }
     }
 }
