e01b3dd576f9642d24d61d862e9887ec6934e7b9
Julian ⚡ <45518053+UtilityDev@users.noreply.github.com>
2024-01-07 18:20:08 +0100

    Add base IEntity interface and empty Player class

---
+++ DeeperDeeper_CS/src/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+namespace DeeperDeeper_CS
+{
+    public interface IEntity
+    {
+        public Vector3 Position { get; set; }
+        public void Draw();
+    }
+}
---
+++ DeeperDeeper_CS/src/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+namespace DeeperDeeper_CS
+{
+    class Player : IEntity
+    {
+        public Vector3 Position { get; set; }
+        public void Draw()
+        {
+
+        }
+    }
+}
