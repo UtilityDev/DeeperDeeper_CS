52f4a12bcc6ba8d27a73453c2c4455435dd446a8
Julian ⚡ <45518053+UtilityDev@users.noreply.github.com>
2024-01-08 07:36:54 +0100

    Add comments in Game.cs

--- DeeperDeeper_CS/src/Game.cs
+++ DeeperDeeper_CS/src/Game.cs
@@ -6,16 +6,21 @@ namespace DeeperDeeper_CS
 {
     public sealed class Game
     {
+        // Window properties
         const int       WIDTH   = 1280;
         const int       HEIGHT  = 720;
         const string    TITLE   = "Deeper & Deeper!";
 
+        // List of blocks
         List<Block> blocks = new List<Block>();
 
+        // Random object
         Random rand = new Random(DateTime.Now.Millisecond);
 
+        // Run game
         public void Run()
         {
+            // Setup window
             InitWindow(WIDTH, HEIGHT, TITLE);
             SetTargetFPS(60);
 
@@ -27,31 +32,39 @@ namespace DeeperDeeper_CS
                 color: Color.RED
             );
 
+            // Spawn 22 blocks
             for (int i = 0; i < 22; i++)
             {
                 blocks.Add(new Block(new Vector2(rand.Next(50, WIDTH - 50), HEIGHT + rand.Next(3, 750)), 100, 300, Color.BLUE));
             }
 
+            // Game loop
             while (!WindowShouldClose())
             {
                 BeginDrawing();
                 ClearBackground(Color.BLACK);
 
+                // Draw player and allow movement
                 player.Draw();
                 player.Move();
 
+                // Loop through every block in 'Blocks' List
                 foreach (Block block in blocks)
                 {
+                    // Draw each block and make them fall
                     block.Draw();
                     block.Fall();
 
+                    // If out of bounds, reset position back down
                     if (block.position.Y < -200)
                         block.position.Y = HEIGHT;
                 }
 
+                // End loop
                 EndDrawing();
             }
 
+            // De-initialize window
             CloseWindow();
         }
     }
