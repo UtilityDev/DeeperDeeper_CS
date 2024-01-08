baa027cfc45e15be7002630499e01f83a4be296b
Julian ⚡ <45518053+UtilityDev@users.noreply.github.com>
2024-01-08 07:38:00 +0100

    Add comments in Entity.cs

--- DeeperDeeper_CS/src/Entity.cs
+++ DeeperDeeper_CS/src/Entity.cs
@@ -6,9 +6,11 @@ namespace DeeperDeeper_CS
 {
     public class Entity
     {
+        // Entity-specific properties
         public Vector2 position = new Vector2();
         public Color color;
 
+        // Draw basic shape, can be overwritten
         public virtual void Draw()
         {
             DrawRectangle((int)position.X, (int)position.Y, 20, 20, Color.WHITE);
