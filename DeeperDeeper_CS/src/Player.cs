16652a63e2b6ac0e466b5b45c19dcda882aa2019
Julian ⚡ <45518053+UtilityDev@users.noreply.github.com>
2024-01-08 07:40:29 +0100

    Add comments in Player.cs

--- DeeperDeeper_CS/src/Player.cs
+++ DeeperDeeper_CS/src/Player.cs
@@ -6,9 +6,11 @@ namespace DeeperDeeper_CS
 {
     class Player : Entity
     {
+        // Player-specific properties
         public float radius = 40.0f;
         public float moveSpeed;
 
+        // Constructor, initialize values
         public Player(Vector2 pos, float radius, float moveSpeed, Color color)
         {
             this.position = pos;
@@ -17,12 +19,15 @@ namespace DeeperDeeper_CS
             this.color = color;
         }
 
+        // Allow player movement
+        // Movement speed is multiplied by 'GetFrameTime()' here to make speed consistent across framerates
         public void Move()
         {
             if (IsKeyDown(KeyboardKey.KEY_A)) { this.position.X -= this.moveSpeed * GetFrameTime(); }
             if (IsKeyDown(KeyboardKey.KEY_D)) { this.position.X += this.moveSpeed * GetFrameTime(); }
         }
 
+        // Overwritten 'Draw()' class, draws a circle
         public override void Draw()
         {
             DrawCircle((int)this.position.X, (int)this.position.Y, this.radius, this.color);
